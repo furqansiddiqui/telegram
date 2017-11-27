a04e465c9ca58c82f1ce6992c299249c2c8fde93
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2017-11-27 14:26:57 +0500

    License update

--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
-#  furqansiddiqui/telegram
+#  Telegram Package
 
-telegram package
+Allow your users to connect Telegram with your app and receive notifications;
+
+## Prerequisites
+
+* PHP 7.0+
+* [**furqansiddiqui**/http-client](https://github.com/furqansiddiqui/http-client) >= 0.2.1
+
+## Installation
+
+`composer require furqansiddiqui/telegram`
--- src/Telegram/TelegramException.php
+++ src/Telegram/TelegramException.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * This file is a part of "furqansiddiqui/telegram" package.
+ * https://github.com/furqansiddiqui/telegram
+ *
+ * Copyright (c) 2017. Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/telegram/blob/master/LICENSE
+ */
+
 declare(strict_types=1);
 
 namespace Telegram;
